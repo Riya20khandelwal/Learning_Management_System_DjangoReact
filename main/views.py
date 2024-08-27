e287a60967bd12641e02097bdcf35e89de8309ed
Riya Khandelwal <riyakhandelwal204@gmail.com>
2024-08-27 21:18:05 +0530

    edit in views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from rest_framework.views import APIView
+from rest_frameork.response import Response
+from rest_framework import generaics
+from .serialzers import TeacherSerializer
+
 
 # Create your views here.
+
+class TeacherList(generaics.ListCreateAPIView):
+    pass
